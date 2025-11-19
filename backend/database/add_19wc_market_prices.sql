fd66db14963481f0dc5325e56af8444da860b467
sun111 <hisamira5284@gmail.com>
2025-11-19 09:58:29 +0900

    Fix 19WC market prices - use overall instead of tier column

--- backend/database/add_19wc_market_prices.sql
+++ backend/database/add_19wc_market_prices.sql
@@ -1,36 +1,32 @@
 -- Add market prices for 19WC (2019 World Championship) season players
--- Price is based on tier multiplier and overall rating
+-- Price is based on overall rating (tier calculated from overall)
 
 INSERT INTO player_market_prices (player_id, base_price, current_price, price_floor, price_ceiling)
 SELECT
   id,
   CASE
-    WHEN tier = 'LEGENDARY' THEN overall * 50
-    WHEN tier = 'EPIC' THEN overall * 30
-    WHEN tier = 'RARE' THEN overall * 20
-    WHEN tier = 'COMMON' THEN overall * 10
-    ELSE overall * 5
+    WHEN overall > 100 THEN overall * 50  -- LEGENDARY
+    WHEN overall > 90 THEN overall * 30   -- EPIC
+    WHEN overall > 80 THEN overall * 20   -- RARE
+    ELSE overall * 10                     -- COMMON
   END as base_price,
   CASE
-    WHEN tier = 'LEGENDARY' THEN overall * 50
-    WHEN tier = 'EPIC' THEN overall * 30
-    WHEN tier = 'RARE' THEN overall * 20
-    WHEN tier = 'COMMON' THEN overall * 10
-    ELSE overall * 5
+    WHEN overall > 100 THEN overall * 50
+    WHEN overall > 90 THEN overall * 30
+    WHEN overall > 80 THEN overall * 20
+    ELSE overall * 10
   END as current_price,
   CASE
-    WHEN tier = 'LEGENDARY' THEN overall * 50 - 200
-    WHEN tier = 'EPIC' THEN overall * 30 - 150
-    WHEN tier = 'RARE' THEN overall * 20 - 100
-    WHEN tier = 'COMMON' THEN overall * 10 - 50
-    ELSE overall * 5 - 25
+    WHEN overall > 100 THEN overall * 50 - 200
+    WHEN overall > 90 THEN overall * 30 - 150
+    WHEN overall > 80 THEN overall * 20 - 100
+    ELSE overall * 10 - 50
   END as price_floor,
   CASE
-    WHEN tier = 'LEGENDARY' THEN overall * 50 + 200
-    WHEN tier = 'EPIC' THEN overall * 30 + 150
-    WHEN tier = 'RARE' THEN overall * 20 + 100
-    WHEN tier = 'COMMON' THEN overall * 10 + 50
-    ELSE overall * 5 + 25
+    WHEN overall > 100 THEN overall * 50 + 200
+    WHEN overall > 90 THEN overall * 30 + 150
+    WHEN overall > 80 THEN overall * 20 + 100
+    ELSE overall * 10 + 50
   END as price_ceiling
 FROM players
 WHERE season = '19WC'
